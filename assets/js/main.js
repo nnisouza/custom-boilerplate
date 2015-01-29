a1179665f9baacf527d63caa7bcc86165e773b57
Giovani <giov@nnisouza.com>
2015-01-29 17:34:50 -0200

    easter egg

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,9 @@ function drawPage() {
     
 }
 function actions() {
-
+    cheet('s h a k e s p e a r e', function () {
+      alert('Doubt thou the stars are fire; \nDoubt that the sun doth move; \nDoubt truth to be a liar; \nBut never doubt I love. \n                                                        - William Shakespeare');
+    });
 }
 function afterLoaded() {
     alert('Go!');
